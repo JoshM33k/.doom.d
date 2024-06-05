6615ae73fe9cb80fc4f11c26b8eb765f1435c403
Josh Meek <JoshM33k@users.noreply.github.com>
2024-06-05 18:02:03 -0400

    New window sizing, splash screens, and new denote shortcuts

--- custom.el
+++ custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "f64189544da6f16bab285747d04a92bd57c7e7813d8c24c30f382f087d460a33" "014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages '(denote)))
 (custom-set-faces
